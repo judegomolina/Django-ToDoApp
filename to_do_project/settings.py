fa087680e152ff5e2d56f129a4e08845b5b5e213
Juan Molina <judegomolina@outlook.com>
2022-01-29 12:01:20 -0400

    Fix - Correcting Allowed Hosts for Heroku Deployment

--- to_do_project/settings.py
+++ to_do_project/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = ['to-do-app-django3.herokuapp.com/', 
+ALLOWED_HOSTS = ['to-do-app-django3.herokuapp.com', 
                 '127.0.0.1']
 
 
